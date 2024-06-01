d08e45cc136bdc4c282c8e6eb4e64d724fbeec7d
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-06-01 11:56:31 -0400

    Created hotel rooms component and objects

---
+++ devhotel/src/components/hotel.js
@@ -0,0 +1,23 @@
+export const hotelRooms = [
+    {
+        roomId: 1,
+        roomType: 'Single',
+        roomPrice: 100,
+        roomCapacity: 1,
+
+    },
+    {
+        roomId: 2,
+        roomType: 'Double',
+        roomPrice: 200,
+        roomCapacity: 2,
+
+    },
+    {
+        roomId: 3,
+        roomType: 'Triple',
+        roomPrice: 300,
+        roomCapacity: 3,
+    }
+
+]
