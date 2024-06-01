487d9e9aaed830a12b77d28942b97e8743143076
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-06-01 12:09:04 -0400

    Updated hotel with table and prop

--- devhotel/src/views/hotel.jsx
+++ devhotel/src/views/hotel.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
-const Hotel = () => {
+const Hotel = ({ hotelRooms }) => {
     return (
         <div>
-        <h1>Hotel Rooms</h1>
+            <h1>Hotel Rooms</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Room ID</th>
+                        <th>Room Type</th>
+                        <th>Room Price</th>
+                        <th>Room Capacity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {hotelRooms.map((room) => (
+                        <tr key={room.roomId}>
+                            <td>{room.roomId}</td>
+                            <td>{room.roomType}</td>
+                            <td>{room.roomPrice}</td>
+                            <td>{room.roomCapacity}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
-    }
-
+}
 
 export default Hotel;
