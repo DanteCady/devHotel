40d7302c9eeaa98534d58e4af18072c93233f277
Dante J Cady <37083142+DanteCady@users.noreply.github.com>
2024-06-05 23:09:28 -0400

    Update App.jsx

--- devhotel/src/App.jsx
+++ devhotel/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hotel from './views/hotel';
+// deatruct data props from hotelrooms
 import { hotelRooms, hotelInfo } from './components/hotelRooms';
 
 const App = () => {
